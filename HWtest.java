710c21e2a7e1838c4d4426c0941b27aab566decd
Shone9558 <167795287+Shone9558@users.noreply.github.com>
2026-04-22 15:47:45 +0800

    Update HWtest.java

--- HWtest.java
+++ HWtest.java
@@ -1,6 +1,6 @@
 public class HW7test {
     public static void main(String[] args) {
        System.out.println("這是用來練習GitHub協作的程式!");
-        System.out.println("\"學號：\"01357027\"\n姓名：\"你的名字\"");
+        System.out.println("\"學號：\"01357027\"\n姓名：\"張筠翔\"");
     }
 }
