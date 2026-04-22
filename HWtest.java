e3fe6ec17457a5fd75d18e0cb0dbb3a9aecf46f2
Wayne-ds <a0973393681@gmail.com>
2026-04-22 15:53:58 +0800

    Add additional student information to HWtest001

--- HWtest.java
+++ HWtest.java
@@ -2,5 +2,7 @@ public class HW7test {
     public static void main(String[] args) {
        System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01357027\"\n姓名：\"張筠翔\"");
+        System.out.println("\"學號：\"01357001\"\n姓名：\"謝龍偉\"");
+        
     }
 }
