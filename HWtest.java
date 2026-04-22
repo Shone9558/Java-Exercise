01bdad0855ba832f1554a13211321a62b5350540
wtrfdc <wu0979601551@gmail.com>
2026-04-22 15:54:41 +0800

    Add student information for 吳泓陞

--- HWtest.java
+++ HWtest.java
@@ -3,6 +3,7 @@ public class HW7test {
        System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01357027\"\n姓名：\"張筠翔\"");
         System.out.println("\"學號：\"01357001\"\n姓名：\"謝龍偉\"");
+        System.out.println("\"學號：\"01357024\"\n姓名：\"吳泓陞\"");
         
     }
 }
